2afd05e2034e0bf700d21ad808d44a24982e8bef
Alaa Ahmed Youssef <master_here100@yahoo.com>
2020-06-21 23:36:30 +0200

    --Add Admin Orders view --Fix in admin css & js & html

--- MdeicalStoreApplication/MedicalStore/Scripts/Custom/admin.js
+++ MdeicalStoreApplication/MedicalStore/Scripts/Custom/admin.js
@@ -1,17 +1,26 @@
-$(document).ready(function(){
-    $(".admins-add").click(function(){
-      $(".catig").css("display", "none");
-      $(".cat").css("display", "none");
-      $(".main .form").css("display", "block");
+$(document).ready(function () {
+    $(".admins-add").click(function () {
+        $(".catig").css("display", "none");
+        $(".cat").css("display", "none");
+        $(".main .form").css("display", "block");
+        $(".order").css("display", "none");
     });
-    $(".categorie-add").click(function(){
+    $(".categorie-add").click(function () {
         $(".cat").css("display", "block");
         $(".catig").css("display", "none");
         $(".main .form").css("display", "none");
-      });
-      $(".products-add").click(function(){
+        $(".order").css("display", "none");
+    });
+    $(".products-add").click(function () {
         $(".catig").css("display", "block");
         $(".cat").css("display", "none");
+        $(".order").css("display", "none");
         $(".main .form").css("display", "none");
-      });
-  });
+    });
+    $(".order-status").click(function () {
+        $(".order").css("display", "block");
+        $(".catig").css("display", "none");
+        $(".cat").css("display", "none");
+        $(".main .form").css("display", "none");
+    });
+});
